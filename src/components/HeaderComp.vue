24261bb8406dd0c8503d9d015e2f280c07be5d0d
GioCri01 <giovanni.crispino94@icloud.com>
2022-05-27 12:14:05 +0200

    Struttura progetto

---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,68 @@
+<template>
+  <header>
+      <div class="header-top"></div>
+      <div class="header-menu d-flex align-items-center">
+          <div class="container d-flex justify-content-between  ">
+              <div class="logo">
+                <img src="../assets/img/logo_48.png" alt="">
+                </div>
+                <div class="menu d-flex">
+                    <ul class="d-flex">
+                        <li><a href="#">link</a></li>
+                        <li><a href="#">link</a></li>
+                        <li><a href="#">link</a></li>
+                        <li><a href="#">link</a></li>
+                        <li><a href="#">link</a></li>
+                    </ul>
+                    <button>bottone</button>
+                </div>
+          </div>
+      </div>
+  </header>
+</template>
+
+<script>
+export default {
+name:"HeaderComp",
+}
+</script>
+
+<style lang="scss" scoped>
+ header{
+     position: fixed;
+     z-index: 999;
+     width: 100%;
+     height: 120px;
+     
+
+     .header-top{
+         height: 40px;
+         background-color: #21333e;
+     }
+
+     .header-menu{
+         height: calc(120px - 40px);
+         ul{
+             margin-right:20px ;
+             list-style: none;
+             li{
+                 padding: 0 30px;
+                 text-transform: uppercase;
+                 a{
+                     color: white;
+                    text-decoration: none;
+                 }
+             }
+         }
+
+         button{
+             padding: 0 30px;
+             border-radius: 10px;
+             text-transform: uppercase;
+             background-color: blue;
+             border: none;
+         }
+     }
+ }
+
+</style>
---
+++ src/components/JumbotronComp.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="jumbotron position-relative">
+    <div class="jumbotron-content ">
+      <h1>Lead ~ Customer</h1>
+      <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ut illum sunt similique eum ad praesentium eligendi! Excepturi, cum quibusdam architecto eum accusamus eveniet eaque ea, repellat rerum nulla corrupti iure!</p>
+      <button>Get in touch</button>
+      <button>Read More</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+ name:"JumbotronComp",
+}
+</script>
+
+<style lang="scss" scoped>
+  .jumbotron{
+    width: 100%;
+    height: 800px;
+    
+    background-image: url(../assets/img/Polygon\ Luminary.svg);
+    background-position: center;
+    background-size:cover ;
+    filter: brightness(0.7);
+
+    .jumbotron-content{
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%,-50%);
+      text-align: center;
+
+    }
+  }
+
+</style>
